1c4adb66de85325fe2bdcec4bd565f223c95221f
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2023-12-08 15:43:17 +0100

    Add Dice class

---
+++ src/main/java/Dice/Dice.java
@@ -0,0 +1,36 @@
+package Dice;
+
+public class Dice {
+
+    private Die die1;
+    private Die die2;
+
+    public Dice() {
+        die1 = new Die();
+        die2 = new Die();
+    }
+
+    public void rollDice() {
+        die1.roll();
+        die2.roll();
+    }
+
+    public int getDie1Value() {
+        return die1.getValue();
+    }
+
+    public int getDie2Value() {
+        return die2.getValue();
+    }
+
+    public int getSum() {
+        rollDice();
+        return getDie1Value() + getDie2Value();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s = %d", getDie1Value(), getDie2Value(), getSum());
+    }
+
+}
