0b14060d834360a7b497e39541e7f9a5f51d6fc5
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2023-12-09 00:43:17 +0100

    Add main menu

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -16,4 +16,40 @@ public class Game {
         gameState = GameState.START;
     }
 
+    public void play() {
+        System.out.println("Craps\n");
+        while (true) {
+            System.out.println("Select an Option");
+            System.out.println("1 - Play");
+            System.out.println("2 - See Rules");
+            System.out.println("3 - Exit");
+            System.out.print("Enter Choice: ");
+
+            int menuChoice = Integer.valueOf(scanner.nextLine());
+
+            if (menuChoice == 1) {
+                while (true) {
+                    System.out.println("1 - Roll Dice");
+                    System.out.println("2 - Quit");
+
+                    int gameChoice = Integer.valueOf(scanner.nextLine());
+
+                    if (gameChoice == 2) {
+                        break;
+                    }
+                }
+                continue;
+            }
+
+            if (menuChoice == 2) {
+                continue;
+            }
+
+            if (menuChoice == 3) {
+                System.out.println("Goodbye!");
+                break;
+            }
+        }
+    }
+
 }
